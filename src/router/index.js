76a3363aa3d3ea1dda1be097664d4bdc16e1935c
TruongToanVN-DEV <truongtoandevvn@gmail.com>
2024-10-24 15:42:15 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -8,17 +8,19 @@ const routes = [
         component: ContactBook,
     },
     
-    // {
-    //     path: '/',
-    //     name: 'contact.add',
-    //     component: () => import("@/components/ContactCard.vue"),  // Thay thế bằng component thực tế của bạn
-    // },
-
     {
         path: "/:pathMatch(.*)*",
         name: "notfound",
         component: () => import("@/views/NotFound.vue"),
     },
+
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
+    },
+
 ];
     const router = createRouter({
         history: createWebHistory(import.meta.env.BASE_URL),
